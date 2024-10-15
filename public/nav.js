98f5913977585017acab4d45c71b1e5129173840
beesilisk <167374291+beesilisk@users.noreply.github.com>
2024-10-14 21:47:26 -0600

    redesign
    a more mobile-friendly layout

---
+++ public/nav.js
@@ -0,0 +1,8 @@
+document.write('\
+<a href="index.html" target="_top"><i class="fa-solid fa-house"></i> homepage </a>\
+<a href="about.html" target="_top"><i class="fa-solid fa-bug"></i> about webmaster </a>\
+<a href="weblog.html" target="_top"><i class="fa-solid fa-pen"></i> weblog </a>\
+<a href="notebook.html" target="_top"><i class="fa-solid fa-laptop-file"></i> notebook </a>\
+<a href="crafts.html" target="_top"><i class="fa-solid fa-scissors"></i> craft catalog </a>\
+<a href="links.html" target="_top"><i class="fa-solid fa-link"></i> links </a>\
+');
---
+++ public/nav_ext.js
@@ -0,0 +1,5 @@
+document.write('\
+<a href="https://neocities.org/site/midnightsolarium" target="_top"><i class="fa-solid fa-helmet-safety"></i> neocities page </a>\
+<a href="https://github.com/beesilisk/midnightsolarium" target="_top"><i class="fa-brands fa-github"></i> source code </a>\
+<a href="https://www.tumblr.com/beesilisk" target="_top"><i class="fa-brands fa-tumblr"></i> tumblr </a>\
+');
