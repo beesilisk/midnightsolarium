0779e1bea9d6da9afdd26be2fdf92c1adbb5971f
beesilisk <167374291+beesilisk@users.noreply.github.com>
2024-11-06 18:46:25 -0700

    recipes

--- public/nav.js
+++ public/nav.js
@@ -4,5 +4,6 @@ document.write('\
 <a href="weblog.html" target="_top"><i class="fa-solid fa-pen"></i> weblog </a>\
 <a href="notebook.html" target="_top"><i class="fa-solid fa-laptop-file"></i> notebook </a>\
 <a href="crafts.html" target="_top"><i class="fa-solid fa-scissors"></i> craft catalog </a>\
+<a href="recipes.html" target="_top"><i class="fa-solid fa-bowl-rice"></i> recipe cards </a>\
 <a href="links.html" target="_top"><i class="fa-solid fa-link"></i> links </a>\
 ');
