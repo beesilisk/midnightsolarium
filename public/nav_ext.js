ecefa0113b8375dc688ded44026fc38ccf9679dc
beesilisk <167374291+beesilisk@users.noreply.github.com>
2024-12-31 16:46:35 -0700

    2024

--- public/nav_ext.js
+++ public/nav_ext.js
@@ -1,5 +1,6 @@
 document.write('\
 <a href="mailto:basilbeeisk@proton.me" target="_top"><i class="fa-solid fa-paper-plane"></i> electronic mail </a>\
 <a href="https://github.com/beesilisk/midnightsolarium" target="_top"><i class="fa-brands fa-github"></i> source code </a>\
-<a href="https://www.tumblr.com/beesilisk" target="_top"><i class="fa-brands fa-tumblr"></i> tumblr </a>\
+<a href="https://spacehey.com/beesilisk" target="_top"><i class="fa-solid fa-user"></i> spacehey</a>\
+<a href="https://www.tumblr.com/beesilisk" target="_top"><i class="fa-brands fa-tumblr"></i> tumblr</a>\
 ');
