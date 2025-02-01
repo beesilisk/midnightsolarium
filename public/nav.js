d64e6a1d5c730f1f52649cd0ead592374c0ae6c4
beesilisk <167374291+beesilisk@users.noreply.github.com>
2025-01-31 21:27:25 -0700

    ruminations

--- public/nav.js
+++ public/nav.js
@@ -2,6 +2,7 @@ document.write('\
 <a href="home.html" target="_top"><i class="fa-solid fa-house"></i> homepage </a>\
 <a href="about.html" target="_top"><i class="fa-solid fa-bug"></i> about webmaster </a>\
 <a href="weblog.html" target="_top"><i class="fa-solid fa-pen"></i> weblog </a>\
+<a href="chaos.html" target="_top"><i class="fa-solid fa-leaf"></i> chaos garden </a>\
 <a href="notebook.html" target="_top"><i class="fa-solid fa-laptop-file"></i> notebook </a>\
 <a href="crafts.html" target="_top"><i class="fa-solid fa-scissors"></i> craft catalog </a>\
 <a href="recipes.html" target="_top"><i class="fa-solid fa-bowl-rice"></i> recipe cards </a>\
